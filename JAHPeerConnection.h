65f0d1931ea616cf06fc7b3ba5f8db4abf7b38cc
Jon Hjelle <jahjelle@gmail.com>
2015-09-01 08:23:00 -0700

    Initial commit

---
+++ JAHPeerConnection.h
@@ -0,0 +1,13 @@
+//
+//  JAHPeerConnection.h
+//
+//  Copyright (c) 2015 Jon Hjelle. All rights reserved.
+//
+
+@import Foundation
+
+@interface JAHPeerConnection : NSObject
+
+
+@end
+
---
+++ JAHPeerConnection.m
@@ -0,0 +1,15 @@
+//
+//  JAHPeerConnection.m
+//
+//  Copyright (c) 2015 Jon Hjelle. All rights reserved.
+//
+
+#import "JAHPeerConnection.h"
+
+@interface JAHPeerConnection ()
+
+@end
+
+@implementation JAHPeerConnection
+
+@end
