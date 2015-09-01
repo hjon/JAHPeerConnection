81e6f92af026269a031a62b848ab916983b27246
Jon Hjelle <jahjelle@gmail.com>
2015-09-01 09:01:47 -0700

    Add basic implementation of JAHPeerConnection as a kind of proxy object for RTCPeerConnection

--- JAHPeerConnection.h
+++ JAHPeerConnection.h
@@ -5,9 +5,29 @@
 //
 
 @import Foundation
+#import "RTCPeerConnection.h"
+
+@class RTCPeerConnectionFactory;
+
+@protocol JAHPeerConnectionDelegate;
+
 
 @interface JAHPeerConnection : NSObject
 
+@property (nonatomic, weak) id<JAHPeerConnectionDelegate> delegate;
+
+- (instancetype)initWithICEServers:(NSArray*)servers constraints:(RTCMediaConstraints*)constraints peerConnectionFactory:(RTCPeerConnectionFactory*)peerConnectionFactory;
 
 @end
 
+
+@protocol JAHPeerConnectionDelegate <NSObject>
+- (void)peerConnection:(JAHPeerConnection*)peerConnection signalingStateChanged:(RTCSignalingState)stateChanged;
+- (void)peerConnection:(JAHPeerConnection*)peerConnection addedStream:(RTCMediaStream*)stream;
+- (void)peerConnection:(JAHPeerConnection*)peerConnection removedStream:(RTCMediaStream*)stream;
+- (void)peerConnectionOnRenegotiationNeeded:(JAHPeerConnection*)peerConnection;
+- (void)peerConnection:(JAHPeerConnection*)peerConnection iceConnectionChanged:(RTCICEConnectionState)newState;
+- (void)peerConnection:(JAHPeerConnection*)peerConnection iceGatheringChanged:(RTCICEGatheringState)newState;
+- (void)peerConnection:(JAHPeerConnection*)peerConnection gotICECandidate:(RTCICECandidate*)candidate;
+- (void)peerConnection:(JAHPeerConnection*)peerConnection didOpenDataChannel:(RTCDataChannel*)dataChannel;
+@end
--- JAHPeerConnection.m
+++ JAHPeerConnection.m
@@ -5,11 +5,67 @@
 //
 
 #import "JAHPeerConnection.h"
+#import "RTCPeerConnectionFactory.h"
+#import "RTCMediaConstraints.h"
+#import "RTCPair.h"
 
-@interface JAHPeerConnection ()
-
+@interface JAHPeerConnection () <RTCPeerConnectionDelegate>
+@property (nonatomic, strong) RTCPeerConnection *peerConnection
 @end
 
+
 @implementation JAHPeerConnection
+
+- (instancetype)initWithICEServers:(NSArray*)servers constraints:(RTCMediaConstraints*)constraints peerConnectionFactory:(RTCPeerConnectionFactory*)peerConnectionFactory {
+	self = [super init];
+	if (self) {
+        if (!constraints) {
+            RTCPair* sctpConstraint = [[RTCPair alloc] initWithKey:@"internalSctpDataChannels" value:@"true"];
+            RTCPair* dtlsConstraint = [[RTCPair alloc] initWithKey:@"DtlsSrtpKeyAgreement" value:@"true"];
+            constraints = [[RTCMediaConstraints alloc] initWithMandatoryConstraints:nil optionalConstraints:@[sctpConstraint, dtlsConstraint]];
+        }
+
+        _peerConnection = [peerConnectionFactory peerConnectionWithICEServers:servers constraints:constraints delegate:self];
+    }
+    return self;
+}
+
+- (id)forwardingTargetForSelector:(SEL)aSelector {
+    return self.peerConnection;
+}
+
+#pragma mark - RTCPeerConnectionDelegate methods
+
+- (void)peerConnection:(RTCPeerConnection*)peerConnection signalingStateChanged:(RTCSignalingState)stateChanged {
+    [self.delegate peerConnection:self signalingStateChanged:stateChanged];
+}
+
+- (void)peerConnection:(RTCPeerConnection*)peerConnection addedStream:(RTCMediaStream*)stream {
+    [self.delegate peerConnection:self addedStream:stream];
+}
+
+- (void)peerConnection:(RTCPeerConnection*)peerConnection removedStream:(RTCMediaStream*)stream {
+    [self.delegate peerConnection:self removedStream:stream];
+}
+
+- (void)peerConnectionOnRenegotiationNeeded:(RTCPeerConnection*)peerConnection {
+    [self.delegate peerConnectionOnRenegotiationNeeded:self];
+}
+
+- (void)peerConnection:(RTCPeerConnection*)peerConnection iceConnectionChanged:(RTCICEConnectionState)newState {
+    [self.delegate peerConnection:self iceConnectionChanged:newState];
+}
+
+- (void)peerConnection:(RTCPeerConnection*)peerConnection iceGatheringChanged:(RTCICEGatheringState)newState {
+    [self.delegate peerConnection:self iceGatheringChanged:newState];
+}
+
+- (void)peerConnection:(RTCPeerConnection*)peerConnection gotICECandidate:(RTCICECandidate*)candidate {
+    [self.delegate peerConnection:self gotICECandidate:candidate];
+}
+
+- (void)peerConnection:(RTCPeerConnection*)peerConnection didOpenDataChannel:(RTCDataChannel*)dataChannel {
+    [self.delegate peerConnection:self didOpenDataChannel:dataChannel];
+}
 
 @end
